8621fed95e9af813bce1ed5d1988bc83e02db20b
Jiapei Liang <liangjiapei@Jiapeis-MacBook-Pro.local>
2016-07-10 08:33:41 -0400

    Break up reducer into multiple reducers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,18 +2,64 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+    name: 'Anonymous',
+    hobbies: [],
+    movies: []
+}
 
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var nameReducer = (state = 'Anonymous', action) => {
     switch(action.type) {
         case 'CHANGE_NAME':
-            return {
-                ...state,
-                name: action.name
-            };
+            return action.name;
         default:
             return state;
     };
 };
+
+var hobbiesReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_HOBBY':
+            return [
+                ...state,
+                {
+                    id: nextHobbyId++,
+                    hobby: action.hobby
+                }
+            ];
+        case 'REMOVE_HOBBY':
+            return state.filter((hobby) => hobby.id !== action.id);
+        default:
+            return state;
+    };
+};
+
+var moviesReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_MOVIE':
+            return [
+                ...state,
+                {
+                    id: nextMovieId++,
+                    title: action.title,
+                    genre: action.genre
+                }
+            ];
+        case 'REMOVE_MOVIE':
+            return state.filter((movie) => movie.id !== action.id);
+        default:
+            return state;
+    };
+};
+
+var reducer = redux.combineReducers({
+    name: nameReducer,
+    hobbies: hobbiesReducer,
+    movies: moviesReducer,
+});
 
 var store = redux.createStore(reducer, redux.compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -25,6 +71,8 @@ var unsubscribe = store.subscribe(() => {
 
     console.log('Name is', state.name);
     document.getElementById('app').innerHTML = state.name;
+
+    console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -34,6 +82,39 @@ console.log('currentState', currentState);
 store.dispatch({
     type: 'CHANGE_NAME',
     name: 'Jiapei'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Running'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Walking'
+});
+
+store.dispatch({
+    type: 'REMOVE_HOBBY',
+    id: 2
+});
+
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Harry Potter',
+    genre: 'Fantasy'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Mad Max',
+    genre: 'Action'
+});
+
+store.dispatch({
+    type: 'REMOVE_MOVIE',
+    id: 1
 });
 
 store.dispatch({
