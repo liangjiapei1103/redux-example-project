d948bc0f6650e94f4ae28c5da0242a15b4b0691f
Jiapei Liang <liangjiapei@Jiapeis-MacBook-Pro.local>
2016-07-10 07:38:50 -0400

    Add reducer and start redux'

---
+++ app/redux-todo-example.jsx
@@ -0,0 +1,18 @@
+var redux = require('redux');
+
+console.log('Starting redux-todo example');
+
+var stateDefault = {
+    searchText: '',
+    showComplated: false,
+    todos: []
+};
+
+var reducer = (state = stateDefault, action) => {
+    return state;
+};
+
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
