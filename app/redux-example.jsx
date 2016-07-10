829fb536b5af4f13a6d4a03fc0c3ec45599e26aa
Jiapei Liang <liangjiapei@Jiapeis-MacBook-Pro.local>
2016-07-10 07:48:18 -0400

    Add support for CHANGE_NAME Action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,10 +3,26 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
-    return state;
+
+    switch(action.type) {
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+            };
+        default:
+            return state;
+    };
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Jiapei'
+});
+
+console.log('Name should be Jiapei', store.getState());
